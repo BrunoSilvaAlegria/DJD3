e81f03ba033849764d74ae821b5e725e6490fca3
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-04-03 21:21:17 +0100

    Rocket Punch (standalone) working
    This version of the rocket punch is prepared to receive a fuel limitation if necessary in the future

--- DJD3/Assets/Scripts/AllRobots/Projectile.cs
+++ DJD3/Assets/Scripts/AllRobots/Projectile.cs
@@ -7,13 +7,24 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        ReplaceObject();
-        Destroy(gameObject);
+        // Check if the collided object has the "Controllable" tag
+        if (collision.gameObject.CompareTag("Controllable"))
+        {
+            Debug.Log("controlled");
+            Destroy(collision.gameObject);  // Destroy the "Controllable" object
+        }
+        else
+        {
+            ReplaceObject();
+        }
+        Destroy(gameObject);  // Destroy the projectile itself
     }
+
     public void ReplaceObject()
     {
         if (replacementPrefab != null)
         {
+            // Use the prefab's rotation rather than whereToSpawn's rotation
             Instantiate(replacementPrefab, whereToSpawn.position, replacementPrefab.transform.rotation);
         }
     }
