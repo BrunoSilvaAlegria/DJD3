64e280f33b1eed34f9db406e3336ca56a064fa79
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-05-06 03:56:19 +0100

    Dismount update

---
+++ DJD3/Assets/Scripts/AllRobots/Dismount.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Dismount : MonoBehaviour
+{
+    public Transform whereToSpawn;
+    private PlayerManager playerManager;
+    public GameObject hand;
+    public GameObject objectToUntag;
+    public GameObject objectToDestroy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            GameObject projectile = Instantiate(hand, whereToSpawn.position, whereToSpawn.rotation);
+            playerManager.currentHealth = 1;
+            Destroy(objectToDestroy);
+                if (objectToUntag != null)
+                    objectToUntag.tag = "Dead";
+                    objectToUntag.layer = LayerMask.NameToLayer("Robot");
+        }
+    }
+}
