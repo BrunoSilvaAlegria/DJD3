f97744145adae7ab2d3b539d7bc8fc0cac27244e
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-05-11 15:29:51 +0100

    Changes to flamethrower

--- DJD3/Assets/Scripts/AllRobots/BurnTrigger.cs
+++ DJD3/Assets/Scripts/AllRobots/BurnTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BurnTrigger : MonoBehaviour
@@ -5,17 +6,42 @@ public class BurnTrigger : MonoBehaviour
     [Tooltip("The layer assigned to robot objects (set in Unity > Layers)")]
     public LayerMask robotLayer;
 
+    [Tooltip("Time (in seconds) a robot must stay in the trigger before burning starts")]
+    public float burnDelay = 3f;
+
+    // Tracks how long each robot has been in the trigger
+    private Dictionary<Collider, float> robotsInTrigger = new Dictionary<Collider, float>();
+
     private void OnTriggerStay(Collider other)
     {
         // Check if the object is in the Robot layer
         if (((1 << other.gameObject.layer) & robotLayer) != 0)
         {
-            RobotStatus robotStatus = other.GetComponent<RobotStatus>();
-
-            if (robotStatus != null)
+            if (!robotsInTrigger.ContainsKey(other))
             {
-                robotStatus.StartOrResetBurn();
+                robotsInTrigger[other] = 0f;
             }
+
+            robotsInTrigger[other] += Time.deltaTime;
+
+            if (robotsInTrigger[other] >= burnDelay)
+            {
+                RobotStatus robotStatus = other.GetComponent<RobotStatus>();
+
+                if (robotStatus != null)
+                {
+                    robotStatus.StartOrResetBurn();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Remove robot from tracking when it leaves the trigger
+        if (robotsInTrigger.ContainsKey(other))
+        {
+            robotsInTrigger.Remove(other);
         }
     }
 }
