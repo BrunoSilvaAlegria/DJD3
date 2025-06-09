f1afc271bc74eaf617b5d996ed9bf8addb8c6915
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-06-09 17:46:29 +0100

    Fully working animations for Hand

--- DJD3/Assets/Scripts/BasicMovement.cs
+++ DJD3/Assets/Scripts/BasicMovement.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-
 public class BasicMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
@@ -9,10 +8,12 @@ public class BasicMovement : MonoBehaviour
     private Rigidbody rb;
     public Animator animator2;
 
+    private string currentAnim = ""; // Tracks the current animation state name
+    private int animLayer = 0;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        animator2 = GetComponent<Animator>();
 
         if (animator2 == null)
         {
@@ -22,48 +23,55 @@ public class BasicMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 velocity = rb.velocity;
+        Vector3 velocity = rb.linearVelocity;
         float rotateDirection = 0f;
 
-        // Movement input
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity = transform.forward * moveSpeed;
-            animator2.SetBool("isWalking", true);
-            animator2.SetBool("isWalking", false);
-            
+        bool walkingInput = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S);
+        string targetAnim = walkingInput ? "Walking" : "Idle";
 
-        }
-        else if (Input.GetKey(KeyCode.S))
+        AnimatorStateInfo stateInfo = animator2.GetCurrentAnimatorStateInfo(animLayer);
+
+        bool sameAnimationPlaying = currentAnim == targetAnim;
+        bool animationFinished = stateInfo.normalizedTime >= 1f;
+
+        if (!sameAnimationPlaying || (sameAnimationPlaying && animationFinished))
         {
-            velocity = -transform.forward * moveSpeed;
-            animator2.SetBool("isWalking", true);
-            animator2.SetBool("isWalking", false);
+            // Only update animator parameters if:
+            // - We're switching to a different animation OR
+            // - We're restarting the same animation but it finished
+
+            if (walkingInput)
+            {
+                animator2.SetBool("isWalking", true);
+                animator2.SetBool("isIdle", false);
+            }
+            else
+            {
+                animator2.SetBool("isWalking", false);
+                animator2.SetBool("isIdle", true);
+            }
+
+            currentAnim = targetAnim;
+        }
+        // else do nothing (same animation playing and not finished)
+
+        // Apply movement velocity
+        if (walkingInput)
+        {
+            velocity = (Input.GetKey(KeyCode.W) ? 1f : -1f) * transform.forward * moveSpeed;
         }
         else
         {
-            // No forward/backward input
             velocity = new Vector3(0f, velocity.y, 0f);
-            animator2.SetBool("isIdle", true);
-            animator2.SetBool("isIdle", false);
-            
-                
         }
 
         // Rotation input
         if (Input.GetKey(KeyCode.A))
-        {
             rotateDirection = -1f;
-        }
         else if (Input.GetKey(KeyCode.D))
-        {
             rotateDirection = 1f;
-        }
 
-        // Apply velocity preserving Y velocity (gravity)
-        rb.velocity = new Vector3(velocity.x, rb.velocity.y, velocity.z);
-
-        // Apply rotation
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
         transform.Rotate(0f, rotateDirection * rotateSpeed * Time.fixedDeltaTime, 0f);
     }
 }
