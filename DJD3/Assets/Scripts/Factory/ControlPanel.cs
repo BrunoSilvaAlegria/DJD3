c65d18e5bd2701b009c9cd136b042a694199b101
TomasCardoso46 <tomascardoso4633@gmail.com>
2025-06-09 10:59:15 +0100

    Finished visual feedback for Factory interactions

--- DJD3/Assets/Scripts/Factory/ControlPanel.cs
+++ DJD3/Assets/Scripts/Factory/ControlPanel.cs
@@ -6,18 +6,22 @@ public class ControlPanel : MonoBehaviour
     public Animator animator;
     private bool doorOpen = false;
     [SerializeField]private bool inRange = false;
+
+    [SerializeField] private GameObject gameObject1;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             inRange = true;
+            gameObject1.active = true;
         }
     }
-    void OggerExit(Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             inRange = false;
+            gameObject1.active = false;
         }
     }
 
@@ -28,6 +32,7 @@ public class ControlPanel : MonoBehaviour
             doorOpen = true;
             Debug.Log("Interact with Player");
             animator.SetTrigger("Open");
+            gameObject1.active = false;
         }   
     }
 }
